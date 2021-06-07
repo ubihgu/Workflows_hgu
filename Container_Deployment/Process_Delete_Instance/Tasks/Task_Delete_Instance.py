f1d03055ce09a5f1de46369ec75b24425ea0135e
ubihgu <>
2021-06-07 16:22:00 +0000

    Updating Workflow on  - 07-06-2021 16:22:00 - ncroot

--- Container_Deployment/Process_Delete_Instance/Tasks/Task_Delete_Instance.py
+++ Container_Deployment/Process_Delete_Instance/Tasks/Task_Delete_Instance.py
@@ -4,6 +4,6 @@ Visit http://[YOUR_MSA_URL]/msa_sdk/ to see what you can import.
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 
-ret = MSA_API.process_content('Instance Deleted', 'Task OK', context, True)
+ret = MSA_API.process_content('Instance Deleted', 'Task OK', True)
 print(ret)
 
